03e8262d1ec7819e660ea280b9f71230dcdc4b48
Siddharth Verma <vermasiddhartha04@gmail.com>
2026-08-18 17:13:39 +0530

    Configure frontend API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "http://127.0.0.1:8000/api/v1",
+
   headers: {
-    "Content-Type": "application/json",
+    Accept: "application/json",
   },
 });
 
